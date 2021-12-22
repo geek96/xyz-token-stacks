ed2b07d710f7f5fb28ebe9547e70c31ecb4e4ac2
Basit Raza <geek96@outlook.com>
2021-12-22 10:47:12 +0400

    added token trasfer test

--- tests/xyz-token_test.ts
+++ tests/xyz-token_test.ts
@@ -1,26 +1,19 @@
 
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that token can be transferred",
     async fn(chain: Chain, accounts: Map<string, Account>) {
+        let deployer = accounts.get("deployer")!;
+        let wallet_1 = accounts.get("wallet_1")!;
         let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+            Tx.contractCall("xyz-token", "transfer", [
+                types.int(10000000000), 
+                types.principal(deployer.address), 
+                types.principal(wallet_1.address),
+                types.none()
+            ], deployer.address),
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
-
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
-});
+        block.receipts[0].result.expectOk().expectBool(true);
+    }
+})
